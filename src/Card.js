69a0545ad636ab09e19bf1e8616c82794bd5bad8
daibeat <daibeat@github.com>
2021-08-15 17:45:19 +1000

    project react files added and first test

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <img src="" alt="moviephoto" />
+      <div>
+        <h2>Jane</h2>
+        <p>janedjfjdklgmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
